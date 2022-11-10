952143d3feac3c1c1cf879dd7c9a2bdd3cad1d25
Tirivashe <tirivasheignatious.ushamba@my.jcu.edu.au>
2022-11-10 22:00:40 +1000

    Initial commit

---
+++ src/week06/Prac3/AdditionQuestion.java
@@ -0,0 +1,14 @@
+package week06.Prac3;
+
+public class AdditionQuestion {   private int a, b;  // The numbers in the problem.
+    public AdditionQuestion() { // constructor
+        a = (int)(Math.random() * 50 + 1);
+        b = (int)(Math.random() * 50);
+    }
+    public String getQuestion() {
+        return "What is " + a + " + " + b + " ?";
+    }
+    public int getCorrectAnswer() {
+        return a + b;
+    }
+}
